20f2b757ce1c74ef24388c08396bf7323d09affa
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-03-12 12:59:30 -0600

    script.js can now change the title of the webpage.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 /* TODO: Build up the HTML document by using JavaScript DOM manipulation functions.
  * Do not rely on changes you may have made to index.html because the grader won't use that file.
  */
+
+ document.title = "Derplandia";
+
+ 
